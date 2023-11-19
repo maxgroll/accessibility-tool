f2478adc4ccb6349a013bdd292419192a17179f3
Max von Groll <maxgroll@gmail.com>
2023-11-19 18:16:09 +0100

    Made changes to util/extract_urls_for_tests.py

--- accessibility_tool/util/extract_urls_for_tests.py
+++ accessibility_tool/util/extract_urls_for_tests.py
@@ -1,4 +1,4 @@
-#all_urls_extractor
+# extract_urls_for_tests.py
 
 import logging
 from urllib.parse import urljoin
@@ -19,16 +19,12 @@ def extract_all_urls(url: str) -> Set[str]:
     This function iteratively extracts URLs from the given base URL and its sub-pages,
     checking each URL for validity and accessibility before adding it to the set of accessible URLs.
     """
-    #logging.info(f"Starting URL extraction from: {url}")
+    logging.info(f"Starting URL extraction from: {url}")
     # Initialize set for storing tested URLs and list for URLs to test
     accessible_urls = set()
 
     # Determine the base URL for resolving relative links
     base_url = urljoin(url, '/')
-
-    # Set up the general directory to store accessibility test results
-    #general_test_dir = 'accessibility_results'
-    #os.makedirs(general_test_dir, exist_ok=True)
 
     # Loop through URLs to extract
     urls_to_extract = {url}  # start with base url
@@ -38,25 +34,24 @@ def extract_all_urls(url: str) -> Set[str]:
         if current_url in accessible_urls or not is_valid_url(current_url, base_url):
             continue
         #add current url to accessible urls
-        #try:
         accessible_urls.add(current_url)
-        #except Exception as e:
-            #logging.error(f"Error occurred while getting URL {current_url}: {e}")
-            #continue  # Optional: Decide whether to continue or break the loop
-
+        logging.info(f"Added {current_url} to accessible URLs")
         # Extract more URLs from the current URL in case there are sub-pages
         try:
+            logging.info(f"Extracting URLs from {current_url}")
             extracted_urls = extract_urls(current_url)
+            logging.info(f"Finished extracting URLs from {current_url}")
 
             # Filter out external URLs and add to the list of URLs to test
             for extracted_url in extracted_urls:
                 full_url = urljoin(base_url, extracted_url)
                 if full_url not in accessible_urls and is_valid_url(full_url, base_url):
                     urls_to_extract.add(full_url)  # Add to set to prevent duplicates
+                    logging.info(f"Added {full_url} to urls_to_extract")
         except Exception as e:
             logging.error(f"Error occurred while extracting URLs from {current_url}: {e}")
 
-    #logging.info(f"Finished URL extraction from: {url}")
+    logging.info(f"Finished URL extraction from: {url}")
     # Return the results
     return accessible_urls
 
