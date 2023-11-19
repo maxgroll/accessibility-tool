853fd3d58d7b24cec8ef63fa85002ebf96c85e28
Max von Groll <maxgroll@gmail.com>
2023-11-19 18:07:10 +0100

    Added constant DATA_DIRECTORY to data/config/__init__.py for easier access

--- accessibility_tool/data/config/__init__.py
+++ accessibility_tool/data/config/__init__.py
@@ -1,4 +1,4 @@
 # data/config/__init__.py
 
 from .config import setup_directories, setup_logging
-from .constants import FULL_LOGS_DIRECTORY, FULL_ACCESSIBILITY_RESULTS_DIRECTORY
+from .constants import FULL_LOGS_DIRECTORY, FULL_ACCESSIBILITY_RESULTS_DIRECTORY, DATA_DIRECTORY
