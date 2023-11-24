6fff171709fb253c4bcaa78278ebc604ebcb209e
Max von Groll <maxgroll@gmail.com>
2023-11-24 21:07:00 +0100

    Refactored util/accessibility_tester.py to a class

--- accessibility_tool/util/accessibility_tester.py
+++ accessibility_tool/util/accessibility_tester.py
@@ -1,60 +1,76 @@
-# accessibility_tester.py
+# util/accessibility_tester.py
 
 import logging
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from axe_selenium_python import Axe
-from typing import Dict, Optional
-from .results_processor import process_accessibility_results
+from typing import Dict, Optional, Set
+from .results_processor import ResultsProcessor
+from util import create_test_directory
 
-def run_accessibility_tests(url: str, test_directory: str) -> Optional[Dict]:
+class AccessibilityTester:
     """
-    Runs accessibility tests on a given URL using Axe with Selenium and saves the results.
+    A class to run accessibility tests on websites using Axe with Selenium.
 
-    Args:
-        url (str): The URL to test for accessibility issues.
-        test_directory (str): The directory where the test results should be saved.
+    This class handles the initialization of the WebDriver, runs Axe accessibility
+    tests on specified URLs, and processes the results.
 
-    Returns:
-        Optional[Dict]: The results of the accessibility tests if successful, None otherwise.
-
-    Raises:
-        Exception: An error occurred while setting up WebDriver or running tests.
+    Attributes:
+        chrome_options (Options): Chrome options for Selenium WebDriver.
     """
-    try:
-        # Set up Chrome options for Selenium
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")  # Ensure GUI is off
-        chrome_options.add_argument("--no-sandbox")  # Bypass OS security model
-        chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
 
-        # Initialize WebDriver
-        driver = webdriver.Chrome(options=chrome_options)
+    def __init__(self):
+        """
+        Initializes the AccessibilityTester with Chrome WebDriver options.
+        """
+        self.test_directory = " "
+        self.chrome_options = Options()
+        self.chrome_options.add_argument("--headless")
+        self.chrome_options.add_argument("--no-sandbox")
+        self.chrome_options.add_argument("--disable-dev-shm-usage")
 
-        # Navigate to the page
-        driver.get(url)
 
-        # Initialize Axe for accessibility testing
-        axe = Axe(driver)
-        axe.inject()
-        results = axe.run()
+    def run_accessibility_tests(self, url: str) -> Optional[Dict]:
+        """
+        Runs accessibility tests on a single URL.
 
-        # Process results (e.g., save to a file or return them)
-        process_accessibility_results(url, results, test_directory)
+        Args:
+            url (str): The URL to test.
 
-        # Quit the driver
-        driver.quit()
+        Returns:
+            Optional[Dict]: The results of the accessibility tests, or None if an error occurs.
+        """
 
-        return results
+        try:
+            driver = webdriver.Chrome(options=self.chrome_options)
+            driver.get(url)
+            axe = Axe(driver)
+            axe.inject()
+            results = axe.run()
+            processor = ResultsProcessor(url, results, self.test_directory)
+            processor.save_results_to_json()
+            processor.save_results_to_csv()
+            driver.quit()
+            return results
+        except Exception as e:
+            logging.error(f"Accessibility testing error for {url}: {e}")
+            return None
 
-    except Exception as e:
-        # Log the error for debugging purposes
-        logging.error(f"Accessibility testing error for {url}: {e}")
-        return None
+    def test_one_url(self, homepage_url: str) -> Optional[Dict[str, Dict]]:
+        self.test_directory = create_test_directory(homepage_url)
+        return {homepage_url: self.run_accessibility_tests(homepage_url)} if homepage_url else None
 
-# This allows the module to be used both as a script and as an importable module
-if __name__ == "__main__":
-    # Example usage:
-    url = "https://example.com"
-    results = run_accessibility_tests(url)
-    print(results)
+    def test_multiple_urls(self, multiple_urls: Set[str]) -> Optional[Dict[str, Dict]]:
+        results = {}
+        for url in multiple_urls:
+            self.test_directory = create_test_directory(url)
+            result = self.run_accessibility_tests(url)
+            if result:
+                results[url] = result
+        return results if results else None
+    
+# Example usage:
+# tester = AccessibilityTester(url)
+# homepage_results = tester.test_homepage("https://example.com")
+# selected_urls_results = tester.test_selected_urls(["https://example.com/page1", "https://example.com/page2"])
+# all_urls_results = tester.test_all_urls({"https://example.com", "https://example.com/about"})
