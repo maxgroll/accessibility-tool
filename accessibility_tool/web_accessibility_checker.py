dedf5b25973194b61100d5e52742c184b34516fd
Max von Groll <maxgroll@gmail.com>
2023-11-24 21:10:24 +0100

    Minor changes to web_accessibility_checker.py

--- accessibility_tool/web_accessibility_checker.py
+++ accessibility_tool/web_accessibility_checker.py
@@ -3,16 +3,16 @@
 import validators
 import logging
 import streamlit as st
-from typing import Optional, List, Dict
 from urllib.parse import urlparse
+from typing import List
 
 from data.config import setup_directories, setup_logging
-from util.accessibility_tester import run_accessibility_tests
-from util import is_url_accessible, create_test_directory
+from util import is_url_accessible
+from util import WebsiteCrawler
+from util import SitemapParser
+from util import AccessibilityTester
 
-from util.website_crawler import WebsiteCrawler
-from util.sitemap_parser import SitemapParser
-
+# Setup logging and directories
 setup_directories()
 setup_logging()
 
@@ -26,7 +26,7 @@ def main():
     """
     st.title("Web Accessibility Checker")
 
-    # Get user input
+    # Get user input: URL and crawl_depth
     with st.form(key='find_urls_form'):
         url = st.text_input(label='Enter the URL of the website to check')
         # Add a number input for crawl depth
@@ -37,21 +37,21 @@ def main():
     if "extracted_urls" not in st.session_state:
         st.session_state.extracted_urls = set()
 
-    ### nuevo
+    # Initialize sessio state for test choice and choice made
     if "test_choice" not in st.session_state:
-        st.session_state.test_choice = "Test all URLs"
+        st.session_state.test_choice = "Test only homepage"
     if "choice_made" not in st.session_state:
         st.session_state.choice_made = False
 
     # Check if Button or enter key was clicked and validate URL
     if find_urls_button or url:
-        #logging.info(f"find_urls Button klicked for: {url}")
         # Check if provided URL is in session state and reset
         if "previous_url" not in st.session_state or st.session_state.previous_url != url:
             st.session_state.extracted_urls = set()  # Reset the URLs if a new URL is entered
             st.session_state.previous_url = url  # Store the new URL
-        # Reset the choice made state to force user to make a choice again
-        st.session_state.choice_made = False
+            # Reset the choice made state to force user to make a choice again
+            st.session_state.choice_made = False
+            st.session_state.test_choice = None
 
         # Validation of provided URL
         if not validators.url(url):
@@ -60,40 +60,34 @@ def main():
         elif not is_url_accessible(url):
             st.error("The URL is not accessible. Please check the URL and try again.")
             return  # Exit the function early
-        
+        ######
+        parsed_url = urlparse(url)
+        base_url = f"{parsed_url.scheme}://{parsed_url.netloc}/"
         # Use the extracted URLs from the session state if available
         if st.session_state.extracted_urls:
             extracted_urls = st.session_state.extracted_urls
-            #st.success("Using previously extracted URLs.")
         else:
             try:
-                logging.info(f"Checking for sitemap on {url}")
+                # Instantiate sitemap parser
+                sitemap_parser = SitemapParser(base_url)
+                # Instantiate crawler
+                crawler = WebsiteCrawler(base_url)
+                logging.info(f"Checking for sitemap on {base_url}")
                 # Try to use the sitemap if it exists
-                sitemap_parser = SitemapParser(url)
                 if sitemap_parser.has_sitemap():
-                    logging.info(f"Sitemap found on {url}: Extracting Urls for Accessibility Tests")
+                    logging.info(f"Sitemap found on {base_url}: Extracting Urls for Accessibility Tests")
                     st.info("Sitemap found. Extracting Urls for Accessibility Tests")
                     # TODO with st.spinner("Extracting URLs from sitemap"):
                     extracted_urls = sitemap_parser.get_sitemap_urls()
                     if not extracted_urls:  # If sitemap parsing returns an empty set
-                        #logging.info(f"Sitemap index found but could not be parsed for {url}. Falling back to crawling.")
+                        # If the sitemap can not be parsed fallback to crawl
                         with st.spinner("Sitemap index found but could not be parsed. Crawling for URLs"):
-                            crawler = WebsiteCrawler(url)
-                            #crawler.crawl(url)
-                            crawler.crawl(url, max_depth=crawl_depth)  # Here you pass the crawl_depth
-                            extracted_urls = crawler.get_crawled_urls()
-                            logging.info(f"Crawling {url} finished")
+                            extracted_urls = crawler.crawl_urls_to_test(base_url, crawl_depth)
                 else:
                     # No sitemap found, start crawling for URLs
                     logging.info(f"No sitemap found on {url}: Crawling for URLs started")
-                    #st.info("No sitemap found. Crawling for URLs started")
                     with st.spinner("No sitemap found. Crawling for URLs"):
-                        crawler = WebsiteCrawler(url)
-                        #crawler.crawl(url)
-                        crawler.crawl(url, max_depth=crawl_depth)  # Here you pass the crawl_depth
-                        extracted_urls = crawler.get_crawled_urls()
-                        logging.info(f"Crawling {url} finished")
-
+                        extracted_urls = crawler.crawl_urls_to_test(base_url, crawl_depth)
                 # Add extracted URLs to session state
                 st.session_state.extracted_urls = extracted_urls
                 st.success("Accessible URLs found. Please select the URLs to test.")
@@ -101,85 +95,72 @@ def main():
                 st.error("An unexpected error occurred. Please try again later.")
                 logging.error(f"Unexpected error during URL extraction: {e}")
                 return
-        st.empty()
-
-#######
+        # Radio button for user to chosse test type
         with st.form(key='test_choice_form'):
             test_choice = st.radio(
-                "Select the URLs to test:",
-                ('Test all URLs', 'Test only homepage', 'Select specific URLs'),
-                index=0  # Standardauswahl ist 'Test all URLs'
-            )
+            "Select the URLs to test:",
+            ('Test only homepage', 'Test all URLs', 'Select specific URLs'),
+            index=None, # Default selection is 'Test only homepage'
+        )
             choice_made = st.form_submit_button(label='Confirm Choice')
-
+            
+        # Update session state based on user's choice
         if choice_made:
             st.session_state.choice_made = True
             st.session_state.test_choice = test_choice
 
-        # Logic based on user's choice (execute only if choice has been made)
+        # Logic based on user's choice
         if st.session_state.choice_made:
+            tester = AccessibilityTester()
             if st.session_state.test_choice == 'Test all URLs':
                 # Logic to test all URLs
-                logging.info(f"Starting accessibility Tests from: {url}")
+                logging.info(f"Starting accessibility Tests from: {base_url}")
                 with st.spinner("Performing accessibility tests"):
                     if extracted_urls:
-                        results: Optional[Dict] = {}
-                    # Loop through the list of selected URLs, create a directory for each URL's test results, and run accessibility tests
-                        for single_url in extracted_urls:
-                            test_directory = create_test_directory(single_url)
-                            results[single_url] = run_accessibility_tests(single_url, test_directory)
-
+                        results = tester.test_multiple_urls(extracted_urls)
+                    
                         if results:
                             st.success("Accessibility tests completed")
-                            logging.info(f"Finished accessibility Tests from: {url} \n {len(extracted_urls)} URLs tested: {extracted_urls}")
+                            logging.info(f"Finished accessibility Tests from: {base_url} \n {len(extracted_urls)} URLs tested: {extracted_urls}")
                         else:
                             st.error("An error occurred while checking the selected URLs.")
                     else:
                         st.error("No URLs selected for testing.")
-            
-            elif st.session_state.test_choice == 'Test only homepage':
-                # Logic to test only the homepage
-                logging.info(f"Starting accessibility Tests from: {url}")
-                parsed_url = urlparse(url)
-                homepage_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
-                with st.spinner("Performing accessibility tests on the homepage"):
-                    results: Optional[Dict] = {}
-                    test_directory = create_test_directory(homepage_url)
-                    results[homepage_url] = run_accessibility_tests(homepage_url, test_directory)
-        
-                    if results:
-                        st.success(f"Accessibility test for the homepage {homepage_url} completed")
-                        logging.info(f"Finished accessibility Test from: {homepage_url}")
+
+            if st.session_state.test_choice == 'Test only homepage':
+                    # Logic to test only the homepage
+                    logging.info(f"Starting accessibility Tests from: {base_url}")
+                    #parsed_url = urlparse(url)
+                    #homepage_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
+                    with st.spinner("Performing accessibility tests on the homepage"):
+                        results = tester.test_one_url(base_url)
+
+                        if results:
+                            st.success(f"Accessibility test for the homepage {base_url} completed")
+                            logging.info(f"Finished accessibility Test from: {base_url}")
+                        else:
+                            st.error("An error occurred while checking the homepage.")
+
+        if st.session_state.extracted_urls and st.session_state.choice_made and st.session_state.test_choice == 'Select specific URLs':
+            # Show multiselect widget for selecting specific URLs
+            with st.form(key='run_tests_form'):
+                selected_urls: List[str] = st.multiselect("Select URLs to test", options=list(extracted_urls), default=None)
+                run_tests_button = st.form_submit_button(label='Run Accessibility Tests')
+
+            # Button to run tests on selected URLs
+            if run_tests_button:
+                logging.info(f"Starting accessibility Tests from: {base_url}")
+                with st.spinner("Performing accessibility tests"):
+                    if selected_urls:
+                        results = tester.test_multiple_urls(selected_urls)
+                    
+                        if results:
+                            st.success("Accessibility tests completed")
+                            logging.info(f"Finished accessibility Tests from: {base_url} \n {len(selected_urls)} URLs tested: {selected_urls}")
+                        else:
+                            st.error("An error occurred while checking the selected URLs.")
                     else:
-                        st.error("An error occurred while checking the homepage.")
-               
-            elif st.session_state.test_choice == 'Select specific URLs':
-                # Form to run tests on selected URLs
-                with st.form(key='run_tests_form'):
-                    # Display URLs as a multiselect widget
-                    selected_urls: List[str] = st.multiselect("Select URLs to test", options=list(extracted_urls), default=list(extracted_urls))
-            
-                    # Button to run tests on selected URLs
-                    run_tests_button = st.form_submit_button(label='Run Accessibility Tests')
-
-                    # if run tests button is clicked
-                    if run_tests_button:
-                        logging.info(f"Starting accessibility Tests from: {url}")
-                        with st.spinner("Performing accessibility tests"):
-                            if selected_urls:
-                                results: Optional[Dict] = {}
-                                # Loop through the list of selected URLs, create a directory for each URL's test results, and run accessibility tests
-                                for single_url in selected_urls:
-                                    test_directory = create_test_directory(single_url)
-                                    results[single_url] = run_accessibility_tests(single_url, test_directory)
-
-                                if results:
-                                    st.success("Accessibility tests completed")
-                                    logging.info(f"Finished accessibility Tests from: {url} \n {len(selected_urls)} URLs tested: {selected_urls}")
-                                else:
-                                    st.error("An error occurred while checking the selected URLs.")
-                            else:
-                                st.error("No URLs selected for testing.")
+                        st.error("No URLs selected for testing.")
                 
 
 if __name__ == "__main__":
