7c13aa0a8fa7c449cf861cf6505e6af9c9cc3be3
Max von Groll <maxgroll@gmail.com>
2023-12-04 18:26:48 +0100

    Optimized util/sitemap_parser.py, added class variables

--- accessibility_tool/util/sitemap_parser.py
+++ accessibility_tool/util/sitemap_parser.py
@@ -14,11 +14,17 @@ class SitemapParser:
     This class handles different formats of sitemaps including those with query parameters.
     It can parse both individual sitemaps and sitemap indexes.
     """
+    # Class variables
+    NAMESPACE = {'sitemap': 'http://www.sitemaps.org/schemas/sitemap/0.9'}
+    IGNORED_SEGMENTS = [
+        'elementor-hf', 'wp-content', 'wp-includes', 'wp-admin', 'feed', 'elementor',
+        'components', 'templates', 'plugins', 'node', 'user', 'catalog', 'author',
+        'checkout', 'customer', 'collections', 'products', 'app', 'site'
+    ]
 
     def __init__(self, base_url: str):
         self.base_url = base_url
         self.sitemap_urls: Set[str] = set()
-        self.namespace = {'sitemap': 'http://www.sitemaps.org/schemas/sitemap/0.9'}
 
     def fetch_sitemap(self, sitemap_url: str) -> Optional[bytes]:
         """
@@ -27,14 +33,16 @@ class SitemapParser:
         try:
             parsed_url = urlparse(sitemap_url)
             if parsed_url.query:
-                base_url = urljoin(sitemap_url, parsed_url.path)
-                params = dict(param.split('=') for param in parsed_url.query.split('&'))
-                response = requests.get(base_url, params=params)
+                base_url = urljoin(sitemap_url, urlparse(sitemap_url).path)
+                params = dict(param.split('=') for param in urlparse(sitemap_url).query.split('&'))
+                response = requests.get(base_url, params=params, timeout=10)
             else:
-                response = requests.get(sitemap_url)
+                response = requests.get(sitemap_url, timeout=10)
 
             if response.status_code == 200:
                 return response.content
+            else:
+                logging.error(f"Fetch sitemap failed with status code: {response.status_code}")
         except requests.exceptions.RequestException as e:
             logging.error(f"An error occurred while fetching the sitemap: {e}")
         except ValueError as e:
@@ -47,7 +55,7 @@ class SitemapParser:
         """
         robots_url = urljoin(self.base_url, 'robots.txt')
         try:
-            response = requests.get(robots_url)
+            response = requests.get(robots_url, timeout=10)
             if response.status_code == 200:
                 for line in response.text.split('\n'):
                     if line.startswith('Sitemap: '):
@@ -66,9 +74,9 @@ class SitemapParser:
             if root.tag != '{http://www.sitemaps.org/schemas/sitemap/0.9}sitemapindex':
                 logging.error("Not a valid sitemap index file.")
                 return  # This is not a sitemap index file
-            sitemap_tags = root.findall('sitemap:sitemap', self.namespace)
+            sitemap_tags = root.findall('sitemap:sitemap', self.NAMESPACE)
             for sitemap in sitemap_tags:
-                loc = sitemap.find('sitemap:loc', self.namespace)
+                loc = sitemap.find('sitemap:loc', self.NAMESPACE)
                 if loc is not None:
                     sitemap_url = loc.text
                     logging.info(f"Found sitemap in index: {sitemap_url}")
@@ -79,8 +87,6 @@ class SitemapParser:
                         logging.error(f"Could not fetch content from sitemap: {sitemap_url}")
         except ET.ParseError as e:
             logging.error(f"An error occurred while parsing the sitemap index content: {e}")
-        except Exception as e:
-            logging.error(f"An unexpected error occurred: {e}")
 
     def parse_sitemap(self, content: bytes) -> None:
         """
@@ -91,17 +97,12 @@ class SitemapParser:
             if root.tag != '{http://www.sitemaps.org/schemas/sitemap/0.9}urlset':
                 logging.error("Not a valid sitemap file.")
                 return  # This is not a sitemap index file
-            ignored_segments = [
-                'elementor-hf', 'wp-content', 'wp-includes', 'wp-admin', 'feed', 'elementor', 
-                'components', 'templates', 'plugins', 'node', 'user', 'catalog',  'author'
-                'checkout', 'customer', 'collections', 'products', 'app', 'site'
-            ]
-            url_tags = root.findall('sitemap:url', self.namespace)
+            url_tags = root.findall('sitemap:url', self.NAMESPACE)
             for url_tag in url_tags:
-                loc = url_tag.find('sitemap:loc', self.namespace)
+                loc = url_tag.find('sitemap:loc', self.NAMESPACE)
                 if loc is not None:
                     url = loc.text
-                    if not any(segment in url.lower().split('/') for segment in ignored_segments):
+                    if not any(segment in url.lower().split('/') for segment in self.IGNORED_SEGMENTS):
                         self.sitemap_urls.add(loc.text)
         except ET.ParseError as e:
             logging.error(f"An error occurred while parsing the sitemap content: {e}")
@@ -114,39 +115,28 @@ class SitemapParser:
         Returns:
             bool: True if a sitemap or sitemap index exists and is well-formed, False otherwise.
         """
-
         # Attempt to fetch sitemap from robots.txt first
         sitemap_url_from_robots = self.fetch_sitemap_from_robots()
         if sitemap_url_from_robots:
             content = self.fetch_sitemap(sitemap_url_from_robots)
             if content:
-                try:
-                    if '<sitemapindex' in content.decode('utf-8'):
-                        self.parse_sitemap_index(content)
-                    else:
-                        self.parse_sitemap(content)
-                    return True if self.sitemap_urls else False
-                except ET.ParseError:
-                    logging.error(f"Parse error encountered for sitemap found in robots.txt: {sitemap_url_from_robots}")
-                    return False
-
+                if '<sitemapindex' in content.decode('utf-8'):
+                    self.parse_sitemap_index(content)
+                else:
+                    self.parse_sitemap(content)
+                return bool(self.sitemap_urls)
         # Fallback to checking for sitemap_index.xml and sitemap.xml
         for sitemap_path in ['sitemap_index.xml', 'sitemap.xml']:
             sitemap_url = urljoin(self.base_url, sitemap_path)
             content = self.fetch_sitemap(sitemap_url)
             if content:
-                try:
-                    if '<sitemapindex' in content.decode('utf-8'):
-                        self.parse_sitemap_index(content)
-                    else:
-                        self.parse_sitemap(content)
-                    if self.sitemap_urls:  # If any URLs were added
-                        return True
-                except ET.ParseError:
-                    logging.error(f"Parse error encountered for {sitemap_path}")
-                    continue  # Continue trying the next sitemap option
-
-        return False  # No sitemaps found or parse error encountered
+                if '<sitemapindex' in content.decode('utf-8'):
+                    self.parse_sitemap_index(content)
+                else:
+                    self.parse_sitemap(content)
+                if self.sitemap_urls:  # If any URLs were added
+                    return True
+        return False # No sitemaps found or parse error encountered
 
     def get_sitemap_urls(self) -> Set[str]:
         """
