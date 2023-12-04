bd9350d2e9c1186622bc773309a11dfdedb68a13
Max von Groll <maxgroll@gmail.com>
2023-12-04 18:20:59 +0100

    Added helpers.can_fetch to util/__init__.py

--- accessibility_tool/util/__init__.py
+++ accessibility_tool/util/__init__.py
@@ -1,7 +1,7 @@
 # util/__init__.py
 
 
-from .helpers import is_url_accessible, is_valid_url, create_test_directory
+from .helpers import is_url_accessible, is_valid_url, create_test_directory, can_fetch
 from .accessibility_tester import AccessibilityTester
 from .results_processor import ResultsProcessor
 from .website_crawler import WebsiteCrawler
