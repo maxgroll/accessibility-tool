8062fd7026993e5fa53e34908b74edec6da7bdbc
Max von Groll <maxgroll@gmail.com>
2024-04-04 18:46:02 +0200

    modified

--- accessibility_tool/util/__init__.py
+++ accessibility_tool/util/__init__.py
@@ -1,8 +1,9 @@
 # util/__init__.py
 
 
-from .helpers import is_url_accessible, is_valid_url, create_test_directory, can_fetch
+from .helpers import is_url_accessible, is_valid_url, create_test_directory, can_fetch, get_latest_results_directory
 from .accessibility_tester import AccessibilityTester
 from .results_processor import ResultsProcessor
 from .website_crawler import WebsiteCrawler
 from .sitemap_parser import SitemapParser
+from .accessibility_report_viewer import AccessibilityReportViewer
