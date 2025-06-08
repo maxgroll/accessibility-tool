23f4bf91d62bf4aa7ad3a5db43226d86e330ec79
Max von Groll <maxgroll@gmail.com>
2025-06-08 23:54:06 +0200

    added new constants to config/__init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,4 +1,4 @@
-# data/config/__init__.py
+# config/__init__.py
 
 from .config import setup_directories, setup_logging
-from .constants import FULL_LOGS_DIRECTORY, FULL_ACCESSIBILITY_RESULTS_DIRECTORY, DATA_DIRECTORY
+from .constants import FULL_LOGS_DIRECTORY, FULL_ACCESSIBILITY_RESULTS_DIRECTORY, DATA_DIRECTORY, AXE_CDN_LATEST, CDNJS_AXE_API
