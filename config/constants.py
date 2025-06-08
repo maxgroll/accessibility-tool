8fe91189cf3a00863997556b626fd4b093b47a49
Max von Groll <maxgroll@gmail.com>
2025-06-08 23:37:12 +0200

    config: add cdnjs API + axe URL template - CDNJS_AXE_API  for latest version lookup - AXE_CDN_LATEST url template for axe.min.js

--- config/constants.py
+++ config/constants.py
@@ -11,3 +11,8 @@ FULL_LOGS_DIRECTORY = os.path.join(DATA_DIRECTORY, LOGS_DIRECTORY)
 FULL_ACCESSIBILITY_RESULTS_DIRECTORY = os.path.join(DATA_DIRECTORY, ACCESSIBILITY_RESULTS_DIRECTORY)
 
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+
+CDNJS_AXE_API = "https://api.cdnjs.com/libraries/axe-core?fields=version"
+AXE_CDN_LATEST = (
+    "https://cdnjs.cloudflare.com/ajax/libs/axe-core/{version}/axe.min.js"
+)
